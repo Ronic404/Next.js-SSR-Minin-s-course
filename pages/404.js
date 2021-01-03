dcce627f0028ec1df882b5030deddb389a79101e
ROnic404 <ronic404@MacBook-Pro-Aleksandr.local>
2021-01-03 16:30:04 +0400

    add ErrorPage & sass

---
+++ pages/404.js
@@ -0,0 +1,12 @@
+import Link from 'next/link'
+import { MainLayout } from '../components/MainLayout'
+import styles from '../styles/error.module.scss'
+
+export default function ErrorPage() {
+  return (
+    <MainLayout>
+      <h1 className={styles.error}>Error Page</h1>
+      <p>Please <Link href={'/'}><a>go back</a></Link> to safety</p>
+    </MainLayout>
+  )
+}
