9de0c3a9d777d4b5206ce4a322c45bac0cb176f3
ROnic404 <ronic404@MacBook-Pro-Aleksandr.local>
2021-01-03 15:26:29 +0400

    add MainLayout & meta tags

--- pages/posts.js
+++ pages/posts.js
@@ -1,3 +1,13 @@
+// import Head from 'next/head';
+import { MainLayout } from '../components/MainLayout';
+
 export default function Posts() {
-  return <h1>Posts page</h1>
+  return (
+    <MainLayout>
+      {/* <Head>
+        <title>Posts page</title>
+      </Head> */}
+      <h1>Posts page</h1>
+    </MainLayout>
+  )
 }
